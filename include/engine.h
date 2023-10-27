b1086cba25d1418aa7eef98e799e075b06d8a23c
Katherine Russell <111341042+KatRuss@users.noreply.github.com>
2023-10-27 21:23:36 +0100

    additions: basic lighting, obj support, painter's algorithm, normal clipping

--- include/engine.h
+++ include/engine.h
@@ -1,5 +1,8 @@
 #include <vector>
 #include <cmath>
+#include <string>
+#include <fstream>
+#include <strstream>
 #include "raylib.h"
 
 // ==========================
@@ -8,11 +11,51 @@
 struct Triangle
 {
     Vector3 points[3];
+    Color col = WHITE;
 };
 
 struct basicMesh
 {
     std::vector<Triangle> tries;
+
+    bool LoadFromObjectFile(std::string filename)
+    {
+        std::ifstream f(filename);
+        if (!f.is_open()) return false;
+
+        //local cache of verts
+        std::vector<Vector3> verts;
+
+        while (!f.eof())
+        {
+            char line[128];
+            f.getline(line,128);
+
+            std::strstream s;
+            s << line;
+
+            char junk;
+
+            if (line[0] == 'v')
+            {
+                Vector3 v;
+                s >> junk >> v.x >> v.y >> v.z;
+                verts.push_back(v);
+            }
+
+            if (line[0] == 'f')
+            {
+                int f[3];
+                s >> junk >> f[0] >> f[1] >> f[2];
+                tries.push_back({verts[f[0]-1],verts[f[1]-1],verts[f[2]-1]});  // Construct triangle and shove it in mesh.
+            }
+        }
+        
+        
+        return true;
+    }
+
+
 };
 
 struct mat4x4{
@@ -41,6 +84,49 @@ mat4x4 getProjectionMatrix(float fFov, float fAspectRatio)
     return matProj;
 }
 
+mat4x4 getRotationMatrix_X(float rotation)
+{
+    mat4x4 matRotX;
+
+    matRotX.m[0][0] = 1;
+    matRotX.m[1][1] = cosf(rotation);
+    matRotX.m[1][2] = sinf(rotation);
+    matRotX.m[2][1] = -sinf(rotation);
+    matRotX.m[2][2] = cosf(rotation);
+    matRotX.m[3][3] = 1;
+
+    return matRotX;
+}
+
+mat4x4 getRotationMatrix_Y(float rotation)
+{
+    mat4x4 matRotY;
+
+    matRotY.m[0][0] = cosf(rotation);
+    matRotY.m[0][2] = sinf(rotation);
+    matRotY.m[1][1] = 1;
+    matRotY.m[2][0] = -sinf(rotation);
+    matRotY.m[2][2] = cosf(rotation);
+    matRotY.m[3][3] = 1;
+
+    return matRotY;
+}
+
+mat4x4 getRotationMatrix_Z(float rotation)
+{
+    mat4x4 matRotZ;
+
+    matRotZ.m[0][0] = cosf(rotation);
+    matRotZ.m[0][1] = sinf(rotation);
+    matRotZ.m[1][0] = -sinf(rotation);
+    matRotZ.m[1][1] = cosf(rotation);
+    matRotZ.m[2][2] = 1;
+    matRotZ.m[3][3] = 1;
+
+    return matRotZ;
+}
+
+// Multiply a Vector 3 by a 4x4 Matrix.
 void MultiplayMatrixVector(Vector3 &input, Vector3 &output, mat4x4 &m)
 {
     output.x = input.x * m.m[0][0] + input.y * m.m[1][0] + input.z * m.m[2][0] + m.m[3][0];
@@ -48,11 +134,26 @@ void MultiplayMatrixVector(Vector3 &input, Vector3 &output, mat4x4 &m)
     output.z = input.x * m.m[0][2] + input.y * m.m[1][2] + input.z * m.m[2][2] + m.m[3][2];
     float w  = input.x * m.m[0][3] + input.y * m.m[1][3] + input.z * m.m[2][3] + m.m[3][3];
 
-    if (w != 0.0f)
+    if (w != 0.0f) // Normalise back into 3 values.
     {
         output.x /= w; output.y /= w; output.z /= w;
     }
-    
+}
+
+
+// ============================
+// ====== TRIANGLE MATHS ======
+// ============================
+
+//Project each point in the triangle into 2D space with projection matrix
+//TODO: Turn this into a generic matrix multiplication function, as this step works on many other things (such as rotation)
+Triangle projectTriangleTo3D(Triangle tri, mat4x4 matProj)
+{
+    Triangle triProjected;
+    MultiplayMatrixVector(tri.points[0],triProjected.points[0],matProj);
+    MultiplayMatrixVector(tri.points[1],triProjected.points[1],matProj);
+    MultiplayMatrixVector(tri.points[2],triProjected.points[2],matProj);
+    return triProjected;
 }
 
 Triangle ApplyTriangleRotation(Triangle &tri, Vector3 rotation)
@@ -61,29 +162,10 @@ Triangle ApplyTriangleRotation(Triangle &tri, Vector3 rotation)
     Triangle triRotatedZ, triRotatedZX, triRotatedZXY;
 
     // == Set Matrixes ==
-    // Z Rotation
-    matRotZ.m[0][0] = cosf(rotation.z);
-    matRotZ.m[0][1] = sinf(rotation.z);
-    matRotZ.m[1][0] = -sinf(rotation.z);
-    matRotZ.m[1][1] = cosf(rotation.z);
-    matRotZ.m[2][2] = 1;
-    matRotZ.m[3][3] = 1;
+    matRotZ = getRotationMatrix_Z(rotation.z);
+    matRotX = getRotationMatrix_X(rotation.x);
+    matRotY = getRotationMatrix_Y(rotation.y);
 
-    // X Rotation
-    matRotX.m[0][0] = 1;
-    matRotX.m[1][1] = cosf(rotation.x);
-    matRotX.m[1][2] = sinf(rotation.x);
-    matRotX.m[2][1] = -sinf(rotation.x);
-    matRotX.m[2][2] = cosf(rotation.x);
-    matRotX.m[3][3] = 1;
-
-    // Y Rotation
-    matRotY.m[0][0] = cosf(rotation.y);
-    matRotY.m[0][2] = sinf(rotation.y);
-    matRotY.m[1][1] = 1;
-    matRotY.m[2][0] = -sinf(rotation.y);
-    matRotY.m[2][2] = cosf(rotation.y);
-    matRotZ.m[3][3] = 1;
 
     // == Apply Rotation ==
     //Z Rotation
@@ -104,16 +186,42 @@ Triangle ApplyTriangleRotation(Triangle &tri, Vector3 rotation)
     return triRotatedZXY;
 }
 
-// ============================
-// ====== TRIANGLE MATHS ======
-// ============================
+// ======================
+// ====== LIGHTING ======
+// ======================
 
-//Project each point in the triangle into 2D space with projection matrix
-Triangle projectTriangleTo3D(Triangle tri, mat4x4 matProj)
+Color getColourFromLuminance(float lum)
 {
-    Triangle triProjected;
-    MultiplayMatrixVector(tri.points[0],triProjected.points[0],matProj);
-    MultiplayMatrixVector(tri.points[1],triProjected.points[1],matProj);
-    MultiplayMatrixVector(tri.points[2],triProjected.points[2],matProj);
-    return triProjected;
+    int brightness = (int)(16.0f*lum);
+    Color col;
+
+    //TODO: Find a better way to derive this
+    switch (brightness)
+    {
+    case 0: col = {15,15,15,255}; break;
+    case 1: col = {30,30,30,255}; break;
+    case 2: col = {45,45,45,255}; break;
+    case 3: col = {60,60,60,255}; break;
+    case 4: col = {75,75,75,255}; break;
+    case 5: col = {90,90,90,255}; break;
+    case 6: col = {105,105,105,255}; break;
+    case 7: col = {120,120,120,255}; break;
+    case 8: col = {135,135,135,255}; break;
+    case 9: col = {150,150,150,255}; break;
+    case 10: col = {165,165,165,255}; break;
+    case 11: col = {180,180,180,255}; break;
+    case 12: col = {195,195,195,255}; break;
+    case 13: col = {210,210,210,255}; break;
+    case 14: col = {225,225,225,255}; break;
+    case 15: col = {240,240,240,255}; break;
+    default: col = BLACK; break;
+    }
+
+    return col;
 }
+
+
+// ==========================
+// ====== MODEL LOADER ======
+// ==========================
+
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "engine.h"
+#include <iostream>
+#include <algorithm>
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -10,34 +12,36 @@ int main(void)
 
     //Test cube - will replace later
     basicMesh meshCube;
-    meshCube.tries = 
-    {
+    // meshCube.tries = 
+    // {
 
-        // SOUTH FACE
-		{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
-		{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+    //     // SOUTH FACE
+	// 	{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+	// 	{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
 
-		// EAST FACE                                             
-		{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
-		{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+	// 	// EAST FACE                                             
+	// 	{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+	// 	{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
 
-		// NORTH FACE                                                   
-		{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
-		{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+	// 	// NORTH FACE                                                   
+	// 	{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+	// 	{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
 
-		// WEST FACE                                                   
-		{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
-		{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+	// 	// WEST FACE                                                   
+	// 	{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+	// 	{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
 
-		// TOP FACE                                                 
-		{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
-		{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+	// 	// TOP FACE                                                 
+	// 	{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+	// 	{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
 
-		// BOTTOM FACE                                                  
-		{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
-		{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+	// 	// BOTTOM FACE                                                  
+	// 	{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+	// 	{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
     
-    };
+    // };
+
+    meshCube.LoadFromObjectFile("models/burger.obj");
 
 
     // Create Screen
@@ -50,6 +54,9 @@ int main(void)
     mat4x4 matProj = getProjectionMatrix(90.0f,fAspectRatio);
     float fTheta = 0.0f;
 
+    Vector3 camera = {0,0,0}; //Super Basic Camera, will be replaced
+
+
     // Main game loop
     //--------------------------------------------------------------------------------------
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -59,7 +66,7 @@ int main(void)
 
 
         fTheta += 1 * GetFrameTime();
-        Vector3 rotator = {fTheta,0.0f,fTheta * 0.5f};
+        Vector3 rotator = {fTheta,fTheta * 0.5f,0.0f};
 
         //----------------------------------------------------------------------------------
 
@@ -69,7 +76,9 @@ int main(void)
 
             ClearBackground(BLACK);
 
-            //Draw Triangles
+            std::vector<Triangle> trianglesToRaster;
+
+            //Get Triangles We Want To Draw
             for (auto tri : meshCube.tries)
             {
                 Triangle triRotated, triTranslated, triProjected;
@@ -78,26 +87,81 @@ int main(void)
 
                 //Translate triangle into view
                 triTranslated = triRotated;
-                triTranslated.points[0].z = triRotated.points[0].z + 3.0f;
-                triTranslated.points[1].z = triRotated.points[1].z + 3.0f;
-                triTranslated.points[2].z = triRotated.points[2].z + 3.0f;
+                triTranslated.points[0].z = triRotated.points[0].z + 5.0f;
+                triTranslated.points[1].z = triRotated.points[1].z + 5.0f;
+                triTranslated.points[2].z = triRotated.points[2].z + 5.0f;
                 
-                // Projection
-                triProjected = projectTriangleTo3D(triTranslated,matProj);
+                // Calculate Normals
+                Vector3 normal, line1, line2;
 
-                //Scale into view
-                triProjected.points[0].x += 1.0f; triProjected.points[0].y += 1.0f;
-                triProjected.points[1].x += 1.0f; triProjected.points[1].y += 1.0f;
-                triProjected.points[2].x += 1.0f; triProjected.points[2].y += 1.0f;
+                line1.x = triTranslated.points[1].x - triTranslated.points[0].x;
+                line1.y = triTranslated.points[1].y - triTranslated.points[0].y;
+                line1.z = triTranslated.points[1].z - triTranslated.points[0].z;
 
-                triProjected.points[0].x *= 0.5 * (float)GetScreenWidth(); triProjected.points[0].y *= 0.5 * (float)GetScreenHeight();
-                triProjected.points[1].x *= 0.5 * (float)GetScreenWidth(); triProjected.points[1].y *= 0.5 * (float)GetScreenHeight();
-                triProjected.points[2].x *= 0.5 * (float)GetScreenWidth(); triProjected.points[2].y *= 0.5 * (float)GetScreenHeight();
+                line2.x = triTranslated.points[2].x - triTranslated.points[0].x;
+                line2.y = triTranslated.points[2].y - triTranslated.points[0].y;
+                line2.z = triTranslated.points[2].z - triTranslated.points[0].z;
 
-                DrawTriangleLines({triProjected.points[0].x,triProjected.points[0].y},
-                            {triProjected.points[1].x,triProjected.points[1].y},
-                            {triProjected.points[2].x,triProjected.points[2].y},
-                            RAYWHITE);
+                normal.x = line1.y * line2.z - line1.z * line2.y;
+                normal.y = line1.z * line2.x - line1.x * line2.z;
+                normal.z = line1.x * line2.y - line1.y * line2.x;
+
+                float normalLength = std::sqrt(normal.x*normal.x + normal.y*normal.y + normal.z*normal.z);
+                normal.x /= normalLength; normal.y /= normalLength; normal.z /= normalLength;
+
+                // Normal Culling
+                if(normal.x * (triTranslated.points[0].x - camera.x) +
+                   normal.y * (triTranslated.points[0].y - camera.y) +
+                   normal.z * (triTranslated.points[0].z - camera.z) < 0.0f) // Accounting for where the camera is positioned, and can actually see. And as a result where the triangle is in 3d space
+                {
+                    // Hyper basic lighting, should be improved later
+                    Vector3 lightDir = {-1.0f,0.0f,-1.0f}; 
+
+                    // For this simple lighting, the more lit a triangle is the more the normal is aligned with the light direction.
+                    //Normalise light normal
+                    float lightNormal = std::sqrt(lightDir.x*lightDir.x + lightDir.y*lightDir.y + lightDir.z*lightDir.z);
+                    lightDir.x /= lightNormal; lightDir.y /= lightNormal; lightDir.z /= lightNormal;            
+
+                    float lightDP = normal.x * lightDir.x + normal.y * lightDir.y + normal.z * lightDir.z;
+           
+
+                    // Projection from 3D -> 2D Screen Space
+                    triProjected = projectTriangleTo3D(triTranslated,matProj);
+                    
+                    //Scale into view
+                    triProjected.points[0].x += 1.0f; triProjected.points[0].y += 1.0f;
+                    triProjected.points[1].x += 1.0f; triProjected.points[1].y += 1.0f;
+                    triProjected.points[2].x += 1.0f; triProjected.points[2].y += 1.0f;
+
+                    triProjected.points[0].x *= 0.5 * (float)GetScreenWidth(); triProjected.points[0].y *= 0.5 * (float)GetScreenHeight();
+                    triProjected.points[1].x *= 0.5 * (float)GetScreenWidth(); triProjected.points[1].y *= 0.5 * (float)GetScreenHeight();
+                    triProjected.points[2].x *= 0.5 * (float)GetScreenWidth(); triProjected.points[2].y *= 0.5 * (float)GetScreenHeight();
+
+                    //Store all the good triangles
+                    triProjected.col = getColourFromLuminance(lightDP);
+                    trianglesToRaster.push_back(triProjected);
+
+
+                }
+            }
+
+            //Sort Valid Triangles - Painter's Algorithm
+            std::sort(trianglesToRaster.begin(),trianglesToRaster.end(),[](Triangle &t1,Triangle &t2)
+            {
+                float z1 = (t1.points[0].z + t1.points[1].z + t1.points[2].z) / 3.0f;
+                float z2 = (t2.points[0].z + t2.points[1].z + t2.points[2].z) / 3.0f;
+                return z1 > z2;
+            });
+
+            //Draw Triangles
+            for (auto &triProjected : trianglesToRaster){
+                {
+                    DrawTriangle({triProjected.points[0].x,triProjected.points[0].y},
+                                {triProjected.points[1].x,triProjected.points[1].y},
+                                {triProjected.points[2].x,triProjected.points[2].y},
+                                 triProjected.col);
+
+                }
             }
             
 
